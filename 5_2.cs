e5f7216e66de8e7fb44cf11ae5c9784d33422b7e
Ира <89978431+Irocchka@users.noreply.github.com>
2022-11-07 14:21:59 +0300

    Add files via upload

---
+++ 5_2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace LR5_2
+{
+    class MyList<T>
+    {
+        public T[] newList = new T[7];
+        int size = 7;
+        int index = 0;
+        public MyList(params T[] tmpList)
+        {
+            newList = new T[tmpList.Count()];
+            size = tmpList.Count();
+            foreach(T elem in tmpList)
+            {
+                newList[index] = elem;
+                index++;
+            }
+        }
+        public T this[int elem]
+        {
+            get=>newList[elem];
+            set=>newList[elem] = value;
+        }
+        public void Add(params T[] tmpList)
+        {
+            if (tmpList.Count() >= size - newList.Count())
+            {
+                T[] timp = new T[newList.Count()];
+                for(int i = 0; i < index; i++)
+                {
+                    timp[i] = newList[i];
+                }
+                newList = new T[tmpList.Count() + size];
+                size = tmpList.Count() + size;
+                for (int i = 0; i < index; i++)
+                {
+                    newList[i] = timp[i];
+                }
+            }
+            foreach (T elem in tmpList)
+            {
+                newList[index] = elem;
+                index++;
+            }
+        }
+        public int Size
+        {
+            get
+            {
+                Console.Write("MyList size: ");
+                return newList.Count();
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write(newList[i]);
+                Console.Write(" ");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>(23, 19, 15, 90, 95, 345);
+            list.Print();
+            Console.Write("\n");
+            Console.WriteLine(list.Size);
+            list.Add(1, 2, 3, 4, 5);
+            list.Print();
+            Console.Write("\n");
+            Console.WriteLine(list.Size);
+            Console.WriteLine(list[3]);
+        }
+    }
+}
---
+++ 5_3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+ class Program
+    {
+        class MyDictionary<TKey, TValue> : IEnumerable
+        {
+            Tuple<TKey, TValue>[] _data;
+            int _size;
+
+            public int Count { get => _size; }
+
+            public MyDictionary(int size)
+            {
+                _data = new Tuple<TKey, TValue>[size];
+                _size = 0;
+            }
+
+            public void Add(TKey key, TValue val)
+            {
+                if (_size == _data.Length)
+                {
+                    Tuple<TKey, TValue>[] newData = new Tuple<TKey, TValue>[_size * 2];
+                    for (int i = 0; i < _size; i++)
+                    {
+                        newData[i] = _data[i];
+                    }
+                    _data = newData;
+                }
+                _data[_size] = new Tuple<TKey, TValue>(key, val);
+                _size++;
+            }
+
+            public Tuple<TKey, TValue> this[int index]
+            {
+                get => _data[index];
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => (IEnumerator)_data.GetEnumerator();
+        }
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Dict = new MyDictionary<int, string>(2);
+            Dict.Add(1, "Meow");
+            Dict.Add(1, "Mrrr");
+            Dict.Add(2, "Frr");
+            Dict.Add(3, "Feow");
+
+            for (int i = 0; i < Dict.Count; i++)
+            {
+                Console.WriteLine(Dict[i]);
+            }
+            Console.WriteLine("=============");
+            foreach (Tuple<int, string> pair in Dict)
+            {
+                Console.WriteLine(pair);
+            }
+
+        }
+    }
